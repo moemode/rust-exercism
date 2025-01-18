c296df0ad76bfb7ddcb7fae5a8cafab1e24d5057
moemode <pfeffer.moritz@gmail.com>
2025-01-18 12:56:13 +0000

    triangle: tuple struct, leave out unnecessary check

--- rust/triangle/src/lib.rs
+++ rust/triangle/src/lib.rs
@@ -1,29 +1,23 @@
 use num_traits::Zero;
 
-pub struct Triangle<T> {
-    a: T,
-    b: T,
-    c: T,
-}
+pub struct Triangle<T> (T, T, T);
 
 impl<T: Ord + Zero + Copy> Triangle<T> {
     pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
         let (a, b, c) = (sides[0], sides[1], sides[2]);
-        if a <= T::zero() || b <= T::zero() || c <= T::zero() {
-            return None;
-        }
         if a + b < c || a + c < b || b + c < a {
             return None;
         }
-        Some(Triangle { a, b, c })
+        // the previous condition implies: a > 0 && b > 0 && c > 0
+        Some(Triangle(a, b, c))
     }
 
     pub fn is_equilateral(&self) -> bool {
-        return self.a == self.b && self.b == self.c;
+        return self.0 == self.1 && self.1 == self.2;
     }
 
     pub fn is_scalene(&self) -> bool {
-        return self.a != self.b && self.a != self.c && self.b != self.c 
+        return self.0 != self.1 && self.0 != self.2 && self.1 != self.2 
     }
 
     pub fn is_isosceles(&self) -> bool {
