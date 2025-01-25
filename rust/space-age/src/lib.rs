19ac56e4e7769f1dde7b8a7fd4beba9f8759aa85
moemode <pfeffer.moritz@gmail.com>
2025-01-25 10:18:10 +0000

    space-age: improve

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,49 +1,35 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug)]
-pub struct Duration(u64);
+pub struct Duration(f64);
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        Duration(s)
+        Duration(s as f64 / 31_557_600.0)
     }
 }
 
 pub trait Planet {
-    const EARTH_YEAR_IN_SECONDS: u64 = 31_557_600;
-
-    fn years_during(d: &Duration) -> f64;
+    fn period() -> f64;
+    fn years_during(d: &Duration) -> f64 {
+        d.0 / Self::period()
+    }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-macro_rules! impl_Planet {
-    ( $( $name:ident => $period:expr ),+ ) => {
-        $(
-            impl Planet for $name {
-                fn years_during(d: &Duration) -> f64 {
-                    d.0 as f64 / (Self::EARTH_YEAR_IN_SECONDS as f64 * $period)
-                }
+macro_rules! planet {
+    ($planet: ident, $period:expr) => {
+        pub struct $planet;
+        impl Planet for $planet {
+            fn period() -> f64 {
+                $period
             }
-        )+
+        }
     };
 }
 
-impl_Planet!(
-    Mercury => 0.2408467,
-    Venus   => 0.61519726,
-    Earth   => 1.0,
-    Mars    => 1.8808158,
-    Jupiter => 11.862615,
-    Saturn  => 29.447498,
-    Uranus  => 84.016846,
-    Neptune => 164.79132
-);
+planet!(Earth, 1.0);
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
