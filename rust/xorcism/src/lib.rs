2413276be9d4694155e616050dfe3f72459eed2f
moemode <pfeffer.moritz@gmail.com>
2025-02-20 19:15:01 +0000

    xorcism: solve

---
+++ rust/xorcism/src/lib.rs
@@ -0,0 +1,52 @@
+use std::{
+    borrow::Borrow,
+    iter::{zip, Cycle},
+    slice::Iter,
+};
+
+/// A munger which XORs a key with some data
+#[derive(Clone)]
+pub struct Xorcism<'a> {
+    // This field is just to suppress compiler complaints;
+    // feel free to delete it at any point.
+    key: Cycle<Iter<'a, u8>>,
+}
+
+impl<'a> Xorcism<'a> {
+    /// Create a new Xorcism munger from a key
+    ///
+    /// Should accept anything which has a cheap conversion to a byte slice.
+    pub fn new<Key: ?Sized + AsRef<[u8]>>(key: &'a Key) -> Xorcism<'a> {
+        Xorcism {
+            key: key.as_ref().iter().cycle(),
+        }
+    }
+
+    /// XOR each byte of the input buffer with a byte from the key.
+    ///
+    /// Note that this is stateful: repeated calls are likely to produce different results,
+    /// even with identical inputs.
+    pub fn munge_in_place(&mut self, data: &mut [u8]) {
+        for (k, d) in zip(&mut self.key, data.iter_mut()) {
+            *d ^= k;
+        }
+    }
+
+    /// XOR each byte of the data with a byte from the key.
+    ///
+    /// Note that this is stateful: repeated calls are likely to produce different results,
+    /// even with identical inputs.
+    ///
+    /// Should accept anything which has a cheap conversion to a byte iterator.
+    /// Shouldn't matter whether the byte iterator's values are owned or borrowed.
+    pub fn munge<'b, Data>(&'b mut self, data: Data) -> impl Iterator<Item = u8> + use<'b, 'a, Data>
+    where
+        Data: IntoIterator,
+        Data::IntoIter: 'b,
+        Data::Item: Borrow<u8>,
+    {
+        data.into_iter()
+            .zip(&mut self.key)
+            .map(|(d, k)| d.borrow() ^ k)
+    }
+}
